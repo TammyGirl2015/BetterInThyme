95101f6e7341c014dbd11659a1037937e8cf920a
Tamarie Maenzanise <mtamarie@yahoo.com>
2024-09-21 14:53:41 +0000

    render models

--- bookings/forms.py
+++ bookings/forms.py
@@ -4,4 +4,4 @@ from .models import Booking
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['table', 'date_time', 'guests']
+        fields = ['table', 'date', 'time', 'guests']
