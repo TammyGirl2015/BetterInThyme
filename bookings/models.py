0d4452c408f920e390926a3b10aa152b346969bc
Tamarie Maenzanise <mtamarie@yahoo.com>
2024-09-21 12:22:56 +0000

    add booking model

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Table(models.Model):
+    number = models.IntegerField()
+    seats = models.IntegerField()
+
+class Booking(models.Model):
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    date_time = models.DateTimeField()
+    guests = models.IntegerField()
