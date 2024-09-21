39a77f50c926037752592279ae6a934b7383aac5
Tamarie Maenzanise <mtamarie@yahoo.com>
2024-09-21 12:33:55 +0000

    remove path names

--- bookings/urls.py
+++ bookings/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import book_table
 
 urlpatterns = [
-    path('book/', book_table, name='book_table'),
+    path('', book_table, name='book_table'),
 ]
--- restaurant_booking/urls.py
+++ restaurant_booking/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('bookings/', include('bookings.urls')),
+    path('', admin.site.urls),
+    path('', include('bookings.urls')),
 ]
