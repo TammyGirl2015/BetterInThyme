61018391c475e084a4e1185c8928a9dcae02f3c0
Tamarie Maenzanise <mtamarie@yahoo.com>
2024-09-21 12:25:49 +0000

    create booking view for bookings

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .forms import BookingForm
+
+def book_table(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Add redirect or success message here
+    else:
+        form = BookingForm()
+    return render(request, 'bookings/book_table.html', {'form': form})
