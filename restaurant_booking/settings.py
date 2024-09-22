682a939a6cca214a9f10f55ecb439b3e1a54e4c4
Tamarie Maenzanise <mtamarie@yahoo.com>
2024-09-22 11:03:43 +0000

    edit settings/ databases

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -84,7 +84,8 @@ WSGI_APPLICATION = 'restaurant_booking.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'))
+
 }
 
 
